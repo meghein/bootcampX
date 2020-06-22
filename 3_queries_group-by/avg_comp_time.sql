ca7a00e846e3f45f9ea8e74492bbb4844777eb99
Meghan Hein <meghan.hein@gmail.com>
2020-06-22 22:01:18 +0000

    queries 3 - group by

---
+++ 3_queries_group-by/avg_comp_time.sql
@@ -0,0 +1,15 @@
+-- SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration
+-- FROM assignment_submissions
+-- JOIN assignments ON assignments.id = assignment_submissions.assignment_id
+-- JOIN students ON students.id = assignment_submissions.student_id
+-- WHERE students.end_date IS NULL
+-- GROUP BY students.name
+-- ORDER BY AVG(assignment_submissions.duration) DESC;
+
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
