3b914f83fa68d032870942833c51917514ef72f0
Meghan Hein <meghan.hein@gmail.com>
2020-06-25 00:08:24 +0000

    parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2] || 'JUL02'
+
+pool.query(queryString, [`%${cohortName}%`])
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
-
-
+  for (const row of res.rows) {
+    const {cohort, teacher} = row;
+    console.log(`${cohort}: ${teacher}`)
+  }
+})
+.catch(e => console.error(e.stack));
